d36a62504ee0fa0c047675df5fc35ebfcab56907
WIN-1Q8DR817JKE\user <aklius14pg@gmail.com>
2025-05-19 19:13:50 +0900

    弾とNeckController追加。腕やりなおす。

---
+++ Assets/Script/NeckController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class NeckController : MonoBehaviour
+{
+    [SerializeField] Transform neck;
+    [SerializeField] Transform sight;
+    // Update is called once per frame
+    void Update()
+    {
+        if (RoundManager.Instance.IsFPS)
+        {
+            neck.LookAt(sight);
+        }
+    }
+}
